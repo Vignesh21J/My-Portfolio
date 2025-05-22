a58b9354b6206e84c4438783685cd92b021df12d
Vignesh J <157782908+Vignesh21J@users.noreply.github.com>
2025-05-22 09:19:55 +0530

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -131,8 +131,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.googlemail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'vrm235678@gmail.com'
-EMAIL_HOST_PASSWORD = 'eyfbliwtweflxfvp'  #app password
+EMAIL_HOST_USER = 'your-email'
+EMAIL_HOST_PASSWORD = 'your-email-password'  #app password
 
 
 # Default primary key field type
